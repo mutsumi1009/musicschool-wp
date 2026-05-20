b5095abb80225bdb82aca30a2d1593c2f42425e0
’mutsumi1009’ <moomoo1009@gmail.com>
2026-05-20 17:36:34 +0900

    5/20修正

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="音楽教室,ギター,ピアノ,ベース,沖縄,宜野湾市">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/common/icon-pc.svg" type="image/svg+xml">
     <?php wp_head(); ?>
 </head>
 
